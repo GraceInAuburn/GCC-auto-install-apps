c591ed4995b71dffcd2f2b456a423bb816eeec6a
wormeyman <wormeyman@gmail.com>
2015-04-14 16:07:21 -0700

    Update for project Move

--- Install.ps1
+++ Install.ps1
@@ -1,8 +1,8 @@
-$source = "https://gist.githubusercontent.com/wormeyman/60e716e197e724ce83a4/raw/a8c108c4915b0f214fad5e184f0180118da3fe06/packages.config"
+$source = "https://github.com/GraceInAuburn/GCC-auto-install-apps/blob/master/packages.config"
 $Filename = [System.IO.Path]::GetFileName($source)
 $dest = "$env:TEMP\$Filename"
 
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($source, $dest)
 
-Start-Process cinst.exe -FilePath $dest -ArgumentList -y
+choco install $dest -y
