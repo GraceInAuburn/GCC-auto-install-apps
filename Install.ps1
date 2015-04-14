a87f54e09bf5a224cc77696e24553a62ddc6c108
wormeyman <wormeyman@gmail.com>
2015-04-14 16:25:05 -0700

    Forgot an equal

--- Install.ps1
+++ Install.ps1
@@ -5,5 +5,5 @@ $dest = "$env:TEMP\$Filename"
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($source, $dest)
 
-$cinstlocation "$env:ProgramData\chocolatey\bin\cinst.exe"
+$cinstlocation = "$env:ProgramData\chocolatey\bin\cinst.exe"
 Start-Process $cinstlocation -FilePath $dest -ArgumentList -y
